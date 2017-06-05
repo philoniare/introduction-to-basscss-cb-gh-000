a0785f963a0db9c3acb4ce20200ce61b07b7807b
Tuguldur Baigalmaa <philoniare@gmail.com>
2017-06-05 04:38:54 +0000

    Done.

---
+++ test/index-test.js
@@ -0,0 +1,12 @@
+const chai = require('chai')
+const fs = require('fs')
+const jsdom = require('mocha-jsdom')
+const path = require('path')
+const spies = require('chai-spies')
+
+
+describe('index', () => {
+  it('passes tests', () => {
+    
+  })
+})
